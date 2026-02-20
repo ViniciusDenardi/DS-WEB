9a00f00770b6720a43cf28ef0fb7a2a03e07a39d
Vinicius <vdenardigalvani9@gmail.com>
2026-02-20 08:12:35 -0300

    Atulizar

---
+++ Front/aula 04/atividade 03/script.js
@@ -0,0 +1,47 @@
+
+var contadorItem = 0;
+
+function adicionar(){
+
+    contadorItem++;
+
+    let novoItem = document.createElement("li");
+    let Nome = document.getElementById("Nome").value;
+    let Telefone = document.getElementById("Telefone").value;
+    let Turma = document.getElementById("Turma").value;
+    let Email = document.getElementById("Email").value;
+    let RM = document.getElementById("RM").value;
+
+ 
+    novoItem.innerHTML =
+    contadorItem + "<br>" +
+    "Nome: " + Nome + "<br>" +
+    "Telefone: " + Telefone + "<br>" +
+    "Turma: " + Turma + "<br>" +
+    "Email: " + Email + "<br>" +
+    "RM: " + RM;
+   
+    novoItem.setAttribute("id", contadorItem);
+
+  
+    let botaoRemover = document.createElement("button");
+    botaoRemover.textContent = "Remover";
+
+ 
+    botaoRemover.setAttribute("onclick", "remover(" + contadorItem + ")");
+    let quebraLinha = document.createElement("br");
+    novoItem.appendChild(quebraLinha);
+    novoItem.appendChild(botaoRemover);
+
+  
+    document.getElementById("lista").appendChild(novoItem);
+    let separador = document.createElement("hr");
+    document.getElementById("lista").appendChild(separador);
+}
+
+
+function remover(id){
+    let item = document.getElementById(id);
+    document.getElementById("lista").removeChild(item);
+}
+ 
